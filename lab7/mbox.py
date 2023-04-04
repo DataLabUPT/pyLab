81589aa3cff18fc7fd844ea56b67236c07dc21bd
razvanvilceanu <40139876+razvanvilceanu@users.noreply.github.com>
2023-04-04 08:40:27 +0000

    added lab 7

---
+++ lab7/mbox.py
@@ -0,0 +1,27 @@
+"""Acest modul contine cod legat de procesare text din fisiere
+Deschide un fisier in format mbox, extrage adresele de mail din FROM,
+afiseaza pe cea mai utilizata
+
+Limbaje de programare 3, Laborator 6, versiunea 2
+de Bogdan Dragulescu
+https://ti.etcti.upt.ro/cursuri/limbaje-de-programare-3/
+
+Copyright 2023 Bogdan Dragulescu, Razvan Vilceanu
+
+Licenta: http://creativecommons.org/licenses/by/4.0/
+"""
+
+from utilfunc import p
+
+# Deschide fisier pentru citire
+# sursa fisier https://mail.python.org/pipermail/
+fhand = open('2018-March.txt')
+p('Fisierul deschis', fhand)
+
+# TODO 5: extrageti adresele de mail de pe linia ce incepe cu 'From ' si numarati cate mailuri au trimis
+
+
+# TODO 6: ordonati adresele de mail descrescator dupa aparitii
+
+
+# TODO 7: afisati adresa de mail cu cele mai multe mesaje trimise si numarul de mesaje
