ee4073340ff0cabd43efc6fb6605d2d23921eb9e
Bogdan <bogdan.dragulescu@upt.ro>
2023-04-13 09:13:06 +0300

    laborator S8

---
+++ lab8/extra.py
@@ -0,0 +1,11 @@
+"""
+1. Găsiți cuvântul cel mai utilizat dintr-un text utilizând doar elemente de programare funcțională.
+"""
+
+
+"""
+2. Procesați un fișier de tip log produs de serverul Apache pentru a obține cel mai utilizat browser pentru acel server 
+utilizând doar elemente de programare funcțională. Exemplu de fișier log:	
+https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/apache_logs/apache_logs
+"""
+
