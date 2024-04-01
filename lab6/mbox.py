a05719df5fae11861250d97887b3e9484a6d5108
razvanvilceanu <40139876+razvanvilceanu@users.noreply.github.com>
2024-04-01 11:31:04 +0000

    added lab6

---
+++ lab6/mbox.py
@@ -0,0 +1,24 @@
+"""Acest modul contine cod legat de liste pentru
+
+Limbaje de programare 3, Laborator 4, vesiunea 1
+de Bogdan Dragulescu, Razvan Vilceanu
+https://datalab.upt.ro/cursuri/limbaje-de-programare-3/
+
+Copyright 2024 Bogdan Dragulescu
+
+Licenta: http://creativecommons.org/licenses/by/4.0/
+"""
+from utilfunc import p
+
+# Deschide fisier pentru citire
+# sursa fisier https://mail.python.org/pipermail/
+fhand = open('2018-March.txt')
+p('Fisierul deschis', fhand)
+
+# TODO 5: extrageti adresele de mail de pe linia ce incepe cu 'From ' si numarati cate mailuri au trimis
+
+
+# TODO 6: ordonati adresele de mail descrescator dupa aparitii
+
+
+# TODO 7: afisati adresa de mail cu cele mai multe mesaje trimise si numarul de mesaje
---
+++ lab6/regexp.py
@@ -0,0 +1,20 @@
+"""Acest modul contine cod legat de liste pentru
+
+Limbaje de programare 3, Laborator 4, vesiunea 1
+de Bogdan Dragulescu, Razvan Vilceanu
+https://datalab.upt.ro/cursuri/limbaje-de-programare-3/
+
+Copyright 2024 Bogdan Dragulescu
+
+Licenta: http://creativecommons.org/licenses/by/4.0/
+"""
+
+from utilfunc import p
+
+import re
+fhand = open('2018-March.txt')
+
+# TODO 8: Afisati liniile care incep cu FROM: utilizand expresii regulate
+
+
+# TODO 9: extrageti toate adresele de mail si numarati aparitiile
