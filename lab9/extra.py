54ee7238e227eee5a9b7c372728f0d6feaa2e1d6
razvanvilceanu <40139876+razvanvilceanu@users.noreply.github.com>
2024-04-22 13:20:14 +0000

    added lab9

---
+++ lab9/extra.py
@@ -0,0 +1,14 @@
+"""
+1. Folosind coloana Context din fișierul logs_lp3_clean.csv filtrați rezultatele din exemplul din laborator doar pentru
+resursele de tip Quiz.
+"""
+
+
+"""
+2. Care sunt datele calendaristice în care au fost cel mai accesate resursele din curs?
+"""
+
+
+"""
+3. Generați grafice pentru rezultatele de la primele două exercițiile
+"""
