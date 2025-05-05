9dde3ca357e2c0cfc89467c807e0e3ff79792a2b
bogdan <dragulescubogdan@gmail.com>
2025-05-05 13:25:26 +0300

    laborator 9

---
+++ lab9/procesare.py
@@ -0,0 +1,62 @@
+"""Acest modul contine cod legat de procesarea si vizualizarea datelor
+utilizand module specializate Python
+
+Pandas doc: http://pandas.pydata.org/pandas-docs/stable/
+Numpy doc: https://docs.scipy.org/doc/numpy/index.html
+Matplotlib doc: https://matplotlib.org
+
+Limbaje de programare 3, Saptamana 10, versiunea 3
+de Bogdan Dragulescu, Razvan Vilceanu
+https://datalab.upt.ro/cursuri/limbaje-de-programare-3/
+
+Copyright 2025 Bogdan Dragulescu
+
+Licenta: http://creativecommons.org/licenses/by/4.0/
+"""
+
+# TODO 1 instalati modulele pandas si matplotlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+from utilfunc import p
+
+# pandas config
+pd.set_option('display.expand_frame_repr', False)
+
+# TODO 2: incarca datele din csv
+
+
+# TODO 3: in ce tip de date a fost incarcat fisierul
+
+
+# TODO 4: informatii despre datele incarcate
+
+
+# TODO 5: afisati primele 10 inregistrari din coloanele 'Time','User','Context'
+
+
+# TODO 6: extrageti contextele sub forma de lista
+
+
+# TODO 7: modifica formatul pentru data in an-luna-zi
+
+
+# TODO 8: pastrati doar intervalul '2025-02-24' - '2025-04-20'
+
+
+# TODO 9: agregati dupa context doar fisierele, numarati accesarile si utilizatorii unici
+
+
+# TODO 10: salveaza rezultatul in csv
+
+
+# TODO 11: afisare rezultate intr-un grafic
+
+
+# TODO 12: schimba etichetele de pe axa x
+
+
+# TODO 13: salveaza imaginea
+
