1463bc2b6551c14c46478d0a3270d2729d4dceba
bogdan <dragulescubogdan@gmail.com>
2026-05-17 13:04:19 +0300

    Laborator 11

---
+++ lab11/concurenta.py
@@ -0,0 +1,36 @@
+"""
+Created on 4/27/2021
+
+@author: razvan.vilceanu
+"""
+
+# TODO 1: importam modulele necesare
+
+
+# TODO 2: Creati un fisier nou func.py in care creati o functie ce descarca si dezarhiveaza fisiere de mail.
+
+
+# TODO 3: rulati functia pentru 2 fisiere:
+"""
+url1 = 'https://mail.python.org/pipermail/flask/2020-March.txt' 
+url2 = 'https://mail.python.org/pipermail/flask/2019-July.txt'
+"""
+url1 = 'https://mail.python.org/pipermail/flask/2020-March.txt'
+url2 = 'https://mail.python.org/pipermail/flask/2019-July.txt'
+
+
+# TODO 4: rulati functia folosind 2 thread-uri
+
+
+# TODO 5: Definiti o functie (in func.py) ce afiseaza frecventa mail-urilor din fisierele descarcate.
+#  Tratati si exceptiile ce pot aparea.
+
+
+# TODO 6: importati si rulati functia pe toate fisierele descarcate si afisati timpul de executie
+
+
+# TODO 7: rulati TODO 6 in thread-uri separate si afisati timpul de executie
+
+
+# TODO 8: rulati TODO 6 folosind procese diferite si afisati timpul executiei
+
